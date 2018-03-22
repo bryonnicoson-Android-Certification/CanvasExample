0c34219dd9f469a59b54347702303ae3e9865b55
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-22 11:01:19 -0500

    touchevent path drawing

--- app/src/main/java/com/bryonnicoson/canvasexample/MyCanvasView.java
+++ app/src/main/java/com/bryonnicoson/canvasexample/MyCanvasView.java
@@ -8,6 +8,7 @@ import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.support.v4.content.res.ResourcesCompat;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -16,6 +17,10 @@ import android.view.View;
 
 public class MyCanvasView extends View {
 
+    // if the finger has barely moved, no need to draw
+    // it is not necessary, using the path, to draw every pixel - interpolate to improve performance
+    private static final float TOUCH_TOLERANCE = 4;
+
     private Paint mPaint;
     private Path mPath;
     private int mDrawColor;
@@ -23,6 +28,8 @@ public class MyCanvasView extends View {
     private Canvas mExtraCanvas;
     private Bitmap mExtraBitmap;
 
+    // path starting points
+    private float mX, mY;
 
     MyCanvasView(Context context) {
         this(context, null);
@@ -67,5 +74,57 @@ public class MyCanvasView extends View {
 
         // draw the bitmap that stores the path the user has drawn
         canvas.drawBitmap(mExtraBitmap, 0, 0, null);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        // invalidate() is within cases because there are many motion events for which we do
+        // not want to redraw screen
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchStart(x, y);
+                // nothing drawn, so no need to invalidate
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touchMove(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touchUp();
+                // nothing drawn, so no need to invalidate
+                break;
+            default:
+                // nada
+        }
+        return true;
+    }
+
+    private void touchStart(float x, float y) {
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    private void touchMove(float x, float y) {
+        // get the distance from current point and start
+        float dx = Math.abs(mX - x);
+        float dy = Math.abs(mY - y);
+        // if distance exceeds tolerance, draw
+        if (dx > TOUCH_TOLERANCE || dy > TOUCH_TOLERANCE) {
+            // QuadTo() adds a quadratic bezier from the last point to the current
+            mPath.quadTo(mX, mY, (mX + x)/2, (mY + y)/2);
+            // reset mX, mY to last drawn point
+            mX = x;
+            mY = y;
+            // save the path in the extra bitmap, which we access through its canvas
+            mExtraCanvas.drawPath(mPath, mPaint);
+        }
+    }
+
+    private void touchUp() {
+        // reset the path so it doesn't get drawn again.
+        mPath.reset();
     }
 }
